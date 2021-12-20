c1efd4193479e361229401108a9777bb899a7abf
ivaan1992 <ivaan.lg.92@gmail.com>
2021-12-19 20:23:05 -0600

    Start the fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: "My first game",
+      result: "My game ID is",
+    }),
+    headers: {
+        'Content-type': 'applications/json; charset=UTF-8',
+    },
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
